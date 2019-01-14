e9422591a21c29a9b29d80ab554e4491056ada94
lhr1 <gpfl104@naver.com>
2019-01-14 17:02:33 +0900

    add 캡슐, main controller script

---
+++ Study/LHR/UnityStudy/UnityStudyProject/Assets/Scripts/MainController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    public int timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("초기화가 이루어졌습니다.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += 1;
+        Debug.Log(timer + "번째 업데이트");
+    }
+}
