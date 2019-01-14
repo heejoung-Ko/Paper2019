4cf2a097386d135234092af3612c1458543e6c80
lhr1 <gpfl104@naver.com>
2019-01-14 17:11:35 +0900

    capsule translate forward

--- Study/LHR/UnityStudy/UnityStudyProject/Assets/Scripts/MainController.cs
+++ Study/LHR/UnityStudy/UnityStudyProject/Assets/Scripts/MainController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class MainController : MonoBehaviour
 {
     public int timer = 0;
+    public GameObject capsule;
+    public float speed = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("초기화가 이루어졌습니다.");
+        capsule = GameObject.Find("Capsule");
     }
 
     // Update is called once per frame
@@ -17,5 +20,7 @@ public class MainController : MonoBehaviour
     {
         timer += 1;
         Debug.Log(timer + "번째 업데이트");
+        capsule.GetComponent<Transform>().Translate(Vector3.forward * speed * Time.deltaTime);
+        //this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 }
