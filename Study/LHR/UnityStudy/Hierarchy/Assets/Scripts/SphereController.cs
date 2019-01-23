b224314f1a2a2cefce71eeb20a16aa4e11df97bc
lhr1 <gpfl104@naver.com>
2019-01-23 17:55:09 +0900

    계층 구조 프로젝트 추가
    space key로 cube 계층 설정

---
+++ Study/LHR/UnityStudy/Hierarchy/Assets/Scripts/SphereController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private bool got = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GameObject child = GameObject.Find("Cube") as GameObject;
+            if (!got)
+            {
+                child.transform.parent = this.transform;
+            }
+            else
+            {
+                child.transform.parent = null;
+            }
+            got = !got;
+        }
+    }
+}
