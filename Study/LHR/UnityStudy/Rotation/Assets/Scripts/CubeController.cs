93ea181a8d226d8578d1b034fbc712fe440acebd
lhr1 <gpfl104@naver.com>
2019-01-24 16:02:06 +0900

    Create Rotation Project

---
+++ Study/LHR/UnityStudy/Rotation/Assets/Scripts/CubeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private float rotationSpeed = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        }
+    }
+}
