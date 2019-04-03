8bdb42e44bf8c354eb1e82fd87416da26fd295d5
heejoung-Ko <hjko0306@gmail.com>
2019-04-03 22:21:07 +0900

    Add EnemyAITest

---
+++ Howling/Assets/Test/EnemyAITest/Script/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public enum EnemyState { idle, trace, escape, attack, die };
+    public EnemyState m_State = EnemyState.idle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
