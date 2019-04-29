b55df422df72b3e6e55297f3a6a4ba5fc0437ae5
natsnatsmon <ha6q6v@gmail.com[merge]>
2019-04-30 02:55:56 +0900

    add Idle arm up

---
+++ Howling/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] // private도 inspector 창에서 보이게하기
+    private float moveSpeed;
+
+    private Rigidbody myRigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
