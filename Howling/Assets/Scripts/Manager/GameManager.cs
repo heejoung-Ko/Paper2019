3b975b3ec9a32e4c8b6c314deddb9a417841806a
heejoung-Ko <hjko0306@gmail.com>
2019-05-07 04:35:01 +0900

    Tree Spaw

--- Howling/Assets/Scripts/Manager/GameManager.cs
+++ Howling/Assets/Scripts/Manager/GameManager.cs
@@ -14,14 +14,14 @@ namespace Howling
 
         public GameObject m_PlayerPrefab;
         public GameObject m_EnemyPrefab;
-        public GameObject m_TreePrefab;
+        public GameObject[] m_TreePrefab;
 
         public PLAYERManager m_Player;
         public EnemyManager[] m_Enemys;
         public TreeManager[] m_Trees;
 
         private static float m_RandomNumber = 10f;
-        private static float m_TreeRandom = 20f;
+        private static float m_TreeRandom = 200f;
 
         public Transform m_EnemySpawn;
         public Transform m_TreeSpawn;
@@ -29,7 +29,6 @@ namespace Howling
         // Start is called before the first frame update
         private void Start()
         {
-            Debug.Log("이거 먼저 불리면 안댐");
             m_StartWait = new WaitForSeconds(m_StartDelay);
             m_EndWait = new WaitForSeconds(m_EndDelay);
 
@@ -64,7 +63,9 @@ namespace Howling
             {
                 Vector3 randomPos = new Vector3(Random.Range(-m_TreeRandom, m_TreeRandom), 0, Random.Range(-m_TreeRandom, m_TreeRandom));
                 Quaternion randomRot = Quaternion.Euler(m_TreeSpawn.rotation.x, m_TreeSpawn.rotation.y + Random.Range(0, 360), m_TreeSpawn.rotation.z);
-                m_Trees[i].m_Instance = Instantiate(m_TreePrefab, m_TreeSpawn.position + randomPos, randomRot) as GameObject;
+                int index = (int)Random.Range(0, 3);
+                Debug.Log(index);
+                m_Trees[i].m_Instance = Instantiate(m_TreePrefab[index], m_TreeSpawn.position + randomPos, randomRot) as GameObject;
                 m_Trees[i].m_TreeNumber = i + 1;
                 m_Trees[i].Setup();
             }
@@ -83,8 +84,7 @@ namespace Howling
         }
 
         private IEnumerator GameStarting()
-        {
-            Debug.Log("이거 나중에 불리면 안댐");
+        { 
             ResetPlayer();
             ResetAllEnemys();
             ResetAllTrees();
