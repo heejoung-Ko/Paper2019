b96fc925dc1983c8364c28e45e36800619eb6ca4
lhr1 <gpfl104@naver.com>
2019-05-12 00:07:16 +0900

    튜토리얼 get item 추가

--- Howling/Assets/Scripts/UI/TutorialController.cs
+++ Howling/Assets/Scripts/UI/TutorialController.cs
@@ -7,14 +7,17 @@ namespace Howling
     public class TutorialController : MonoBehaviour
     {
         private GameObject currentTutorial = null;
-        private float startTime = 2f;
-        private float deleteTime = 3f;
+        private float startTime = 1f;
+        private float readyTime = 2f;
+        private float deleteTime = 0.5f;
+        private float delayDeleteTime = 3f;
 
-        public int currentShow = 0;
+        [HideInInspector] public int currentShow = 0;
         private int maxShow = 6;
-        public bool isPlayerMove = false;
-        public bool isPlayerRun = false;
-        public bool isPlayerRotation = false;
+        [HideInInspector] public bool isPlayerMove = false;
+        [HideInInspector] public bool isPlayerRun = false;
+        [HideInInspector] public bool isPlayerRotation = false;
+        [HideInInspector] public bool isPlayerGetItem = false;
 
         private void Start()
         {
@@ -38,12 +41,14 @@ namespace Howling
                 case 3:
                     yield return ShowRotation();
                     break;
+                case 4:
+                    yield return ShowGetItem();
+                    break;
             }
             if (currentShow < maxShow)
-                StartCoroutine(TutorialLoop(startTime));
+                StartCoroutine(TutorialLoop(readyTime));
             else
                 Debug.Log("튜토리얼 끝!");
-            Debug.Log("currentShow - " + currentShow);
         }
 
         private IEnumerator ShowMove()
@@ -54,7 +59,7 @@ namespace Howling
             {
                 yield return null;
             }
-            yield return StartCoroutine(DelayTime(0.5f));
+            yield return StartCoroutine(DelayTime(deleteTime));
         }
 
         private IEnumerator ShowRun()
@@ -65,7 +70,7 @@ namespace Howling
             {
                 yield return null;
             }
-            yield return StartCoroutine(DelayTime(0.5f));
+            yield return StartCoroutine(DelayTime(deleteTime));
         }
 
         private IEnumerator ShowRotation()
@@ -73,6 +78,17 @@ namespace Howling
             currentTutorial = transform.GetChild(currentShow).gameObject;
             currentTutorial.SetActive(true);
             while (!isPlayerRotation)
+            {
+                yield return null;
+            }
+            yield return StartCoroutine(DelayTime(delayDeleteTime));
+        }
+
+        private IEnumerator ShowGetItem()
+        {
+            currentTutorial = transform.GetChild(currentShow).gameObject;
+            currentTutorial.SetActive(true);
+            while (!isPlayerGetItem)
             {
                 yield return null;
             }
