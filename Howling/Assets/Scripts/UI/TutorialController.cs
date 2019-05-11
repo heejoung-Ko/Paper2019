4d0957d1248f2835f739dd2f7ef9b2891f6dd42d
lhr1 <gpfl104@naver.com>
2019-05-11 13:06:51 +0900

    튜토리얼 ui 추가, TutorialController 추가 (start, move)

---
+++ Howling/Assets/Scripts/UI/TutorialController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Howling
+{
+    public class TutorialController : MonoBehaviour
+    {
+        private float startTime = 1f;
+        private float moveTime = 1f;
+        private GameObject currentTutorial = null;
+
+        private void Start()
+        {
+            StartCoroutine(StartTutorial());
+        }
+
+        private IEnumerator StartTutorial()
+        {
+            yield return StartCoroutine(DelayTime(startTime));
+            yield return ShowMove();
+        }
+
+        private IEnumerator ShowMove()
+        {
+            currentTutorial = transform.FindChild("Tutorial_Move").gameObject;
+            currentTutorial.SetActive(true);
+            yield return StartCoroutine(DelayTime(moveTime));
+        }
+
+        private IEnumerator DelayTime(float deleyTime)
+        {
+            yield return new WaitForSeconds(deleyTime);
+            if (currentTutorial != null)
+            {
+                currentTutorial.SetActive(false);
+                currentTutorial = null;
+            }
+        }
+    }
+}
