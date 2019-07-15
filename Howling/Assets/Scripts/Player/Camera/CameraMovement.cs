ae8b1bb43ded311e51223ee304f161b552d91afb
heejoung-Ko <hjko0306@gmail.com>
2019-07-16 03:38:27 +0900

    Player: Camera conect model head bone

---
+++ Howling/Assets/Scripts/Player/Camera/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // 모델링의 머리 본 위치
+    public GameObject headJoint;
+
+    void Awake()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = headJoint.transform.position;
+    }
+}
