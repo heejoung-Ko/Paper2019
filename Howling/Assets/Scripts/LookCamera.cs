62fcc1b4a6f59e95434959bb1ad9887dfe7d2ed3
natsnatsmon <ha6q6v@gmail.com[merge]>
2019-07-26 13:43:00 +0900

    Create Terrarium Environment

---
+++ Howling/Assets/Scripts/LookCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookCamera : MonoBehaviour
+{
+    public float mouseSensitivityX = 5.0f;
+    public float mouseSensitivityY = 5.0f;
+    float rotY = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GetComponent<Rigidbody>())
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+        }    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            float rotX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivityX;
+
+            rotY += Input.GetAxis("Mouse Y") * mouseSensitivityY;
+            rotY = Mathf.Clamp(rotY, -89.5f, 89.5f);
+            transform.localEulerAngles = new Vector3(-rotY, rotX, 0.0f);
+        }
+
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * .1f;
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.forward * .1f;
+        }
+
+        if(Input.GetKey(KeyCode.U))
+        {
+            gameObject.transform.localPosition = new Vector3(0.0f, 50.0f, 0.0f);
+            transform.localEulerAngles = new Vector3(90f, 0.0f, 0.0f);
+        }
+    }
+}
---
+++ Howling/Assets/Scripts/Plant.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    [Header("Plant Points (30 Max)")]
+    public float maxEnergy;
+    public float matureSize;
+    public float growthRate;
+    public float seedSpreadRadius;
+
+    [Header("Monitoring")]
+    public float energy;
+    public float size;
+    public float age;
+
+    [Header("Seedling")]
+    public GameObject seedingSpawn;
+
+    [Header("Species Parameters")]
+    public float energyGrowthRate = .01f;
+    public float ageRate = .001f;
+    private Transform Environment;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = 1;
+        energy = 1;
+        age = 0;
+        Environment = transform.parent;
+        TransformSize();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (CanGrow) Grow();
+        if (CanReproduce) Reproduce();
+        if (Dead) Destroy(this);
+        age += ageRate;
+        energy += energyGrowthRate;
+    }
+
+    void TransformSize()
+    {
+        transform.localScale = Vector3.one * size;
+    }
+
+    bool CanGrow
+    {
+        get
+        {
+            return energy > ((maxEnergy / 2) + 1);
+        }
+    }
+
+    bool CanReproduce
+    {
+        get
+        {
+            if (size >= matureSize && CanGrow) return true;
+            else return false;
+        }
+    }
+
+    bool Dead
+    {
+        get
+        {
+            return energy < 0 || age > matureSize;
+        }
+    }
+
+    void Grow()
+    {
+        if (size > matureSize) return;
+        energy = energy / 2;
+        size += growthRate * Random.value;
+        TransformSize();
+    }
+
+    void Reproduce()
+    {
+        var vec = Random.insideUnitCircle * seedSpreadRadius
+            + new Vector2(transform.position.x, transform.position.z);
+        Instantiate(seedingSpawn, new Vector3(vec.x, 0, vec.y), Quaternion.identity, Environment);
+        energy = energy / 2;
+    }
+}
