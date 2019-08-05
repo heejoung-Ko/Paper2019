e11ce82666ac75c0faadc481255f3994907abf73
lhr1 <gpfl212@kpu.ac.kr>
2019-08-06 03:06:01 +0900

    add TestEnemyManager

---
+++ Howling/Assets/Scripts/ML/MLTestEnemyManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MLTestEnemyManager : MonoBehaviour
+{
+    public Transform m_SpawnPoint;
+    [HideInInspector] public GameObject m_Instance;
+
+    private MLTestEnemy m_TestEnemy;
+
+    public void Setup()
+    {
+        m_TestEnemy = m_Instance.GetComponent<MLTestEnemy>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
