a93b06f0ee35ee15eea8e6b97a7f2f64a9a08b52
lhr1 <gpfl212@kpu.ac.kr>
2019-08-06 03:05:46 +0900

    update TestEnemy move

--- Howling/Assets/Scripts/ML/MLTestEnemy.cs
+++ Howling/Assets/Scripts/ML/MLTestEnemy.cs
@@ -6,16 +6,36 @@ public class MLTestEnemy : MonoBehaviour
 {
     public float Hp;
     public float AttackDamage;
+    public Vector3 direction;
+    public float velocity;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (0 >= Hp)
+        {
+            //gameObject.SetActive(false);
+            Destroy(gameObject);
+            return;
+        }
+
+        Move();
+    }
+
+    // TOOD: DeadZone 닿으면 방향 바꾸기
+
+    private void Move()
+    {
+        transform.position = new Vector3(transform.position.x + direction.x * velocity * Time.deltaTime,
+                                                    transform.position.y,
+                                                    transform.position.z + direction.z * velocity * Time.deltaTime);
+
+        Quaternion newRotation = Quaternion.LookRotation(direction);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 5.0f);
     }
 }
