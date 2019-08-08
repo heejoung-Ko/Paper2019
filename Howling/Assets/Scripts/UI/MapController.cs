a93673be77f71ef16e9e01e09cdcfebb263fb735
heejoung-Ko <hjko0306@gmail.com>
2019-08-09 03:32:49 +0900

    Add Map'

---
+++ Howling/Assets/Scripts/UI/MapController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject playerPointer;
+
+    int mapWidth = 350;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(playerPointer.transform.localPosition);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pointerPosition = new Vector3(player.transform.position.x / 350 * 250, player.transform.position.z / 350 * 250);
+        playerPointer.transform.localPosition = pointerPosition;
+
+        Quaternion pointerRotation = new Quaternion(0, 0, -player.transform.rotation.y, player.transform.rotation.w);
+        playerPointer.transform.rotation = pointerRotation;
+
+    }
+}
---
+++ Howling/Assets/Scripts/UI/UIManagerController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagerController : MonoBehaviour
+{
+    // 플레이어 움직임 끄기 위해
+    public GameObject PlayerController;
+
+    // 창 끄고 켜기 위해
+    public GameObject MakingUI;
+    public GameObject MapUI;
+
+    enum UIState
+    {
+        NONE, MAKING, MAP
+    };
+
+    UIState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = UIState.NONE;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            // 조합 상태 일때
+            if (state == UIState.MAKING)
+            {
+                exitMaking();
+            }
+            // 아무 상태도 아닐때
+            else if(state == UIState.NONE)
+            {
+                enterMaking();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            // 지도 상태 일때
+            if (state == UIState.MAP)
+            {
+                exitMap();
+            }
+            // 아무 상태도 아닐때
+            else if (state == UIState.NONE)
+            {
+                enterMap();
+            }
+        }
+    }
+
+    void enterMaking()
+    {
+        state = UIState.MAKING;
+
+        enterUI();
+
+        MakingUI.SetActive(true);
+    }
+
+    void exitMaking()
+    {
+        state = UIState.NONE;
+
+        exitUI();
+
+        MakingUI.GetComponent<Making>().UnsetRecipe();
+        MakingUI.SetActive(false);
+    }
+
+    void enterMap()
+    {
+        state = UIState.MAP;
+
+        MapUI.SetActive(true);
+    }
+
+    void exitMap()
+    {
+        state = UIState.NONE;
+
+        MapUI.SetActive(false);
+    }
+
+    void enterUI()
+    {
+        PlayerController.GetComponent<Howling.PlayerMoveScript>().enabled = false;
+        PlayerController.GetComponent<Howling.PlayerAtk>().enabled = false;
+        PlayerController.GetComponentInChildren<Howling.PlayerHandController>().enabled = false;
+    }
+
+    void exitUI()
+    {
+        PlayerController.GetComponent<Howling.PlayerMoveScript>().enabled = true;
+        PlayerController.GetComponent<Howling.PlayerAtk>().enabled = true;
+        PlayerController.GetComponentInChildren<Howling.PlayerHandController>().enabled = true;
+    }
+}
