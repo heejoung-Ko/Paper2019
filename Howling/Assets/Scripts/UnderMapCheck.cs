e8998078bdc5f49b4418b9bbc8dbf36d8a25864d
lhr1 <gpfl212@kpu.ac.kr>
2019-08-17 20:23:14 +0900

    update under the map check

--- Howling/Assets/Scripts/UnderMapCheck.cs
+++ Howling/Assets/Scripts/UnderMapCheck.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class UnderMapCheck : MonoBehaviour
 {
+    public float replaceHeight = 13f;
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name + " - UnderMap trigger enter, destroy!");
-        Destroy(other.gameObject);
+        Debug.Log(other.name + " - UnderMap trigger enter!");
+
+        if (other.tag == "item")
+        {
+            Vector3 itemPos = other.gameObject.transform.position;
+            other.gameObject.transform.position = new Vector3(itemPos.x, replaceHeight, itemPos.z);
+        }
+        else if (other.tag == "agent")
+        {
+            WolfAgent agent = other.gameObject.GetComponent<WolfAgent>();
+            agent.Done();
+            agent.AgentReset();
+        }
+        else if (other.tag == "enemyCollider")
+        {
+            Enemy enemy = other.gameObject.GetComponentInParent<Enemy>();
+            enemy.state = Enemy.EnemyState.die;
+        }
+        else
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
