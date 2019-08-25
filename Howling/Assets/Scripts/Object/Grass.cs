f73c55b1f8d65ee9fb5f1dc6aa2abc9319b73d1f
natsnatsmon <ha6q6v@gmail.com>
2019-08-25 11:32:42 +0900

    Add Pawn Plant

---
+++ Howling/Assets/Scripts/Object/Grass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+    public GameObject[] grassPrefabs;
+    public float groundRange;
+    public int num;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < num; i++)
+        {
+            GameObject obj = Instantiate(grassPrefabs[Random.Range(0, grassPrefabs.Length)]) as GameObject;
+            obj.transform.position = new Vector3(Random.Range(-groundRange, groundRange), 0, Random.Range(-groundRange, groundRange));
+            obj.transform.parent = transform;
+        }
+    }
+}
